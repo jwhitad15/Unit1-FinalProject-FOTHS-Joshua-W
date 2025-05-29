eb54c7e393b942d5a73dc26e69cd666db737310f
Joshua <jwhite83193@gmail.com>
2025-05-29 10:50:09 -0500

    "Halt on Construction, Deploying Site"

--- FOTHS/src/components/foths-main-page.jsx
+++ FOTHS/src/components/foths-main-page.jsx
@@ -25,19 +25,19 @@ const FOTHSMain = () => {
 
                 <div className="fruits-grid">
                     <div className="fruit-cards-row-1">
-                        <a href="./game-mode" id="card-hyperlink" className="fruit-card-1">Faith</a>
-                        <a href="./game-mode" id="card-hyperlink" className="fruit-card-2">Fruit 2</a>
-                        <a href="./game-mode" id="card-hyperlink" className="fruit-card-3">Fruit 3</a>
+                        <a href="./game-mode" id="card-hyperlink" className="fruit-card-1">Love</a>
+                        {/* <a href="./game-mode" id="card-hyperlink" className="fruit-card-2">Joy</a> */}
+                        {/* <a href="./game-mode" id="card-hyperlink" className="fruit-card-3">Peace</a> */}
                     </div>
                     <div className="fruit-cards-row-2">
-                        <a href="./game-mode" id="card-hyperlink" className="fruit-card-4">Fruit 4</a>
-                        <a href="./game-mode" id="card-hyperlink" className="fruit-card-5">Fruit 5</a>
-                        <a href="./game-mode" id="card-hyperlink" className="fruit-card-6">Fruit 6</a>
+                        <a href="./game-mode" id="card-hyperlink" className="fruit-card-4">longsuffering</a>
+                        {/* <a href="./game-mode" id="card-hyperlink" className="fruit-card-5">Fruit 5</a> */}
+                        {/* <a href="./game-mode" id="card-hyperlink" className="fruit-card-6">Fruit 6</a> */}
                     </div>
                     <div className="fruit-cards-row-3">
                         <a href="./game-mode" id="card-hyperlink" className="fruit-card-7">Fruit 7</a>
-                        <a href="./game-mode" id="card-hyperlink" className="fruit-card-8">Fruit 8</a>
-                        <a href="./game-mode" id="card-hyperlink" className="fruit-card-9">Fruit 9</a>
+                        {/* <a href="./game-mode" id="card-hyperlink" className="fruit-card-8">Fruit 8</a> */}
+                        {/* <a href="./game-mode" id="card-hyperlink" className="fruit-card-9">Fruit 9</a> */}
                     </div>
                 </div>
 
