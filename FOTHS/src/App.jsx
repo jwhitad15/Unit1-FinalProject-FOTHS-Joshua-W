56173f325a12a6bb3c193ef1966d46873586d9a6
Joshua White <jwhite83193@gmail.com>
2025-05-31 20:48:16 -0500

    trying hashrouter again

--- FOTHS/src/App.jsx
+++ FOTHS/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import {Routes, Route } from 'react-router-dom'
+import {Routes, Route, HashRouter } from 'react-router-dom'
 import LoginForm from './components/login-form'
 import SignupForm from './components/signup-form';
 import Dashboard from './components/dashboard';
@@ -14,19 +14,21 @@ function App() {
  
   return (
     <>
-      <Routes>
-        <Route path="/" element={ <Navigate to="/#/dashboard" replace/>} />
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={ <Navigate to="/#/dashboard" replace/>} />
 
-        <Route path="#/login" element={ <LoginForm/>} />
-        <Route path="#/signup" element={ <SignupForm/>} />
-        <Route path="#/dashboard" element={ <Dashboard/>} />
-        <Route path="#/foths" element={ <FOTHSMain/>} />
-        <Route path="#/game-mode" element={ <GameMode/>} />
-        <Route path="#/study" element={ <Study/>} />
-        <Route path="#/about" element={ <About/>} />
-        <Route path="#/under-construction" element={ <UnderConstruction/>} />
-        <Route path="*" element={<ErrorPage/>} />
-      </Routes>
+          <Route path="#/login" element={ <LoginForm/>} />
+          <Route path="#/signup" element={ <SignupForm/>} />
+          <Route path="#/dashboard" element={ <Dashboard/>} />
+          <Route path="#/foths" element={ <FOTHSMain/>} />
+          <Route path="#/game-mode" element={ <GameMode/>} />
+          <Route path="#/study" element={ <Study/>} />
+          <Route path="#/about" element={ <About/>} />
+          <Route path="#/under-construction" element={ <UnderConstruction/>} />
+          <Route path="*" element={<ErrorPage/>} />
+        </Routes>
+      </HashRouter>
     </>
   )
 }
