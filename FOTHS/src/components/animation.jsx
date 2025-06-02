94b129b6095b3b5345801f1a8cc019add809296f
Joshua <jwhite83193@gmail.com>
2025-06-02 01:34:50 -0500

    Dashboard Media Queries Set

--- FOTHS/src/components/animation.jsx
+++ FOTHS/src/components/animation.jsx
@@ -2,7 +2,7 @@
 
 const Animation = () => {
 
-        return <div id="scrolling2"> <p class="paragraph"> "Study to shew thyself approved unto God."</p> </div>;
+        return <div id="scrolling2"> <p className="paragraph"> "Study to shew thyself approved..."</p> </div>;
     };
 
 <br />
--- FOTHS/src/components/start-foths.jsx
+++ FOTHS/src/components/start-foths.jsx
@@ -16,19 +16,24 @@ const StartFoths = () => {
 
 
     return (
-        <div className="foths-start" >
-            <h1 className="header">FOTHS</h1>
-            <p id="login-title">Fruits of the Holy Spirit</p>
 
-            <form className="login-form" onSubmit={handleSubmit}>
+        <div>
 
-            <h4 className="start-page-text">To start the application, press the button below!</h4>
-                <a href="./#/dashboard" class="button-class" text="Hover Effect 2" effecttype="effect2" >Start</a> 
-               
-            </form> <br />
+            <div className="foths-start" >
+        
+                <h1 className="header">FOTHS</h1>
+                <p id="login-title">Fruits of the Holy Spirit</p>
 
-            {/* Scripture Animation */}
-            <Animation/>
+                <form className="login-form" onSubmit={handleSubmit}>
+
+                <h4 className="start-page-text">To start the application, press the button below!</h4>
+                    <a href="./#/dashboard" className="button-class" text="Hover Effect 2" effecttype="effect2" >Start</a> 
+                
+                </form> <br />
+                </div>
+
+                {/* Scripture Animation */}
+                <Animation/>
 
         </div>
         
